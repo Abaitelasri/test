1cabcb85337a55f71a28ac1b10762cb1035a9307
Anas Agouriane <anas.agouriane@it.devcorp.fr>
2023-04-02 14:08:38 +0000

    new

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
+const port = 3000;
 
+// Configure body-parser middleware to parse JSON data
+app.use(bodyParser.json());
+
+// Define the endpoint for receiving webhook notifications
+app.post('/webhook', (req, res) => {
+  // Log the incoming data to the console
+  console.log(req.body);
+
+  // Send a response back to Kibana
+  res.send('OK');
+});
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
-
-app.post('/webhook', (req, res) => {
-  console.log(req.body);
-  res.sendStatus(200);
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+// Start the server
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
